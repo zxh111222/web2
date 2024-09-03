0efd28a2ab5d02027ebf33ab8173856338b10f9f
zxhhhxz1221 <1305894626@qq.com>
2024-09-03 23:18:03 +0800

    新建第一个 Servlet

--- src/main/java/io/github/zxh111222/web2/MyFirstServlet.java
+++ src/main/java/io/github/zxh111222/web2/MyFirstServlet.java
@@ -1,7 +1,35 @@
 package io.github.zxh111222.web2;
 
-import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
 
-public class MyFirstServlet extends HttpServlet {
+import java.io.IOException;
 
+@WebServlet("/my-first-servlet")
+public class MyFirstServlet implements Servlet {
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("MyFirstServlet.service");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "";
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
