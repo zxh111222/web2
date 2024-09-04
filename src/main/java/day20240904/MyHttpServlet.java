415e7bcec7d87201e13747a061a5fdf95ac33c32
zxhhhxz1221 <1305894626@qq.com>
2024-09-04 21:18:43 +0800

    增加 login.html 页面，同时增加 login Servlet，在 Servlet 中获取用户输入的内容...
    method="get"的方式，会把数据显示在浏览器的地址栏，不安全

--- src/main/java/day20240904/MyHttpServlet.java
+++ src/main/java/day20240904/MyHttpServlet.java
@@ -12,13 +12,16 @@ import java.io.IOException;
 public class MyHttpServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
-        System.out.println("MyHttpServlet.doGet");
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        System.out.println("MyHttpServlet.doGet:username=" + username + ",password=" + password);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
         System.out.println("MyHttpServlet.doPost");
     }
 }
