50350c338ade2b6c51009fd3f01a270470cddaac
zxhhhxz1221 <1305894626@qq.com>
2024-09-04 20:59:20 +0800

    MyHttpServlet extends HttpServlet, 指定URL

---
+++ src/main/java/day20240904/MyHttpServlet.java
@@ -0,0 +1,24 @@
+package day20240904;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/login")
+public class MyHttpServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        System.out.println("MyHttpServlet.doGet");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+        System.out.println("MyHttpServlet.doPost");
+    }
+}
