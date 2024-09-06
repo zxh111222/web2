524d92d8498f04c6ce05a80b08b69b46e9b244f3
zxhhhxz1221 <1305894626@qq.com>
2024-09-06 11:48:20 +0800

    MyHttpServlet 判断登录是否成功框架 (err)

--- src/main/java/day20240904/MyHttpServlet.java
+++ src/main/java/day20240904/MyHttpServlet.java
@@ -7,9 +7,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/login")
 public class MyHttpServlet extends HttpServlet {
+
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/login_test";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASSWORD = "123456";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String username = req.getParameter("username");
@@ -23,5 +29,23 @@ public class MyHttpServlet extends HttpServlet {
         String password = req.getParameter("password");
 
         System.out.println("MyHttpServlet.doPost:username=" + username + ",password=" + password);
+
+        boolean isValUser = valUser(username,password);
+
+        resp.setContentType("text/html;charset=UTF-8");
+
+        PrintWriter writer = resp.getWriter();
+        if (isValUser) {
+            登录成功
+        } else {
+            登录失败
+        }
+
+    }
+
+    private boolean valUser(String username, String password) {
+        boolean isVal = false;
+
+        return isVal;
     }
 }
