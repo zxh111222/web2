ed1767a49a952ecc05f7e84df08bd070b71e509e
zxhhhxz1221 <1305894626@qq.com>
2024-09-08 11:15:15 +0800

    课堂作业: LangServlet

---
+++ src/main/java/day20240908/LangServlet.java
@@ -0,0 +1,28 @@
+package day20240908;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/browser_language")
+public class LangServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter writer = resp.getWriter();
+        if ((req.getHeader("accept-language")).contains("zh-CN")) {
+            writer.println("<h1>肚子饿了！</h1>");
+        }else if ((req.getHeader("accept-language")).contains("en")) {
+            writer.println("<h1>Hello World!</h1>");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
