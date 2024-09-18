189ac17c3458edf99626f116be3dc19d9be07254
zxhhhxz1221 <1305894626@qq.com>
2024-09-18 20:23:22 +0800

    day20240918 - RequestServlet4.java

---
+++ src/main/java/day20240918/RequestServlet4.java
@@ -0,0 +1,32 @@
+package day20240918;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/RequestServlet4")
+public class RequestServlet4 extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter writer = response.getWriter();
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        writer.write("username:" + username + System.lineSeparator());
+        writer.write("password:" + password + System.lineSeparator());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter writer = response.getWriter();
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        writer.write("doPost:username=" + username);
+
+    }
+}
