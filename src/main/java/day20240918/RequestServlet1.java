9eed11a1355c0adf146fd7e4d5c4ef495299d6ae
zxhhhxz1221 <1305894626@qq.com>
2024-09-18 20:03:27 +0800

    day20240918 - RequestServlet1.java

---
+++ src/main/java/day20240918/RequestServlet1.java
@@ -0,0 +1,22 @@
+package day20240918;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/RequestServlet1")
+public class RequestServlet1 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter writer = response.getWriter();
+        String name = request.getParameter("name");
+        writer.write("<h1>你好，"+ name +"</h1>");
+    }
+
+}
